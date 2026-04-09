6d2e8b8f93e72d0ba7560a1610e76da4fab67354
Mehmet Magden <mehmetmagden.it@gmail.com>
2026-04-09 08:15:16 -0400

    feat: Summon Heimdall and the Sacred Tests

---
+++ app/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+# db
+bikes =[
+    {"id":1, "model": "Scott Sub Active", "status": "available"},
+    {"id":2, "model": "Specialized Turbo", "status": "rented"}
+]
+
+@app. route('/health')
+def health():
+    return jsonify({"status": "healty", "service": "Aare Bike Rental"}), 200
+
+@app.route('/bikes')
+def get_bikes():
+    return jsonify(bikes), 200
+
+if __name__ == '__main__'
+    app.run(host='0.0.0.0', port=5000)
